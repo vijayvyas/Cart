c9bbea09d79a3d53ac71805cf325fb67c340db37
Vijay Vyas <vijay.bhilwara@gmail.com>
2017-09-30 01:12:14 +0530

    added more seed data

--- src/main/java/com/shopping/cart/configuration/DataSeed.java
+++ src/main/java/com/shopping/cart/configuration/DataSeed.java
@@ -15,8 +15,12 @@ public class DataSeed {
 	@Bean
 	CommandLineRunner init(CartRepository cartRepository) {
 		return (evt) -> {
-			cartRepository.save(new Cart(1, "Item1", 1, 10.0, "/item/1", 123));
-			cartRepository.save(new Cart(2, "Item2", 2, 20.0, "/item/2", 123));
+			cartRepository.save(new Cart(1, "Hitachi Ac 1.5 Ton", 1, 10.0, "/item/1", 123));
+			cartRepository.save(new Cart(2, "Refregirator 360 Ltr", 2, 20.5, "/item/2", 123));
+			cartRepository.save(new Cart(3, "Sony TV 55 Inch", 3, 10.0, "/item/3", 123));
+			cartRepository.save(new Cart(4, "Washing Maching 8 ltr", 4, 20.20, "/item/4", 123));
+			cartRepository.save(new Cart(5, "Bad King size", 5, 20.0, "/item/5", 123));
+			cartRepository.save(new Cart(6, "Sofa 3 by 2", 6, 10.0, "/item/6", 123));
 		};
 	}
 }
